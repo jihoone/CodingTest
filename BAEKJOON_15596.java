8e2ef37c29888cc2e88c4faca05a8a20731b72e9
jihoone <jungjihoons@naver.com>
2020-10-20 17:23:40 +0900

    BAEKJOON_15596번

---
+++ BAEKJOON_15596.java
@@ -0,0 +1,13 @@
+package BAEKJOON;
+
+public class BAEKJOON_15596 {
+	long sum(int[] a) {
+		long ans = 0;
+		
+		for (int i = 0; i < a.length; i++) {
+			ans += a[i];
+		}
+		
+		return ans;
+	}
+}
