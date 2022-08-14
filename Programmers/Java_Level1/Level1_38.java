5e80f5bce5bc50b8ad4e36e7654dd185710e3990
정지훈 <jungjihoons@naver.com>
2022-08-14 22:16:00 +0900

    문자열을 정수로 바꾸기

---
+++ Programmers/Java_Level1/Level1_38.java
@@ -0,0 +1,13 @@
+//	문자열을 정수로 바꾸기
+
+package coodingTest;
+
+public class Level1_38 {
+	class Solution {
+	    public int solution(String s) {
+	        int answer = Integer.parseInt(s);
+	        
+	        return answer;
+	    }
+	}
+}
