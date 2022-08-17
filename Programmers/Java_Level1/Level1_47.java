7bd0949fc35322b89a2677db41b6f4d0756d7415
정지훈 <jungjihoons@naver.com>
2022-08-17 21:15:46 +0900

    짝수와 홀수

---
+++ Programmers/Java_Level1/Level1_47.java
@@ -0,0 +1,12 @@
+//	짝수와 홀수
+
+package coodingTest;
+
+public class Level1_47 {
+	class Solution {
+	    public String solution(int num) {
+	        if(num % 2 == 0) return "Even";
+	        return "Odd";
+	    }
+	}
+}
