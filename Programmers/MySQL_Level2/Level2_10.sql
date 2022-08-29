7805da63d1b67f5d1b308667add6f0ea5acf7199
정지훈 <jungjihoons@naver.com>
2022-08-29 23:13:22 +0900

    중성화 여부 파악하기

---
+++ Programmers/MySQL_Level2/Level2_10.sql
@@ -0,0 +1,10 @@
+--  중성화 여부 파악하기
+
+-- 코드를 입력하세요
+SELECT ANIMAL_ID, NAME,
+    CASE
+        WHEN SEX_UPON_INTAKE LIKE "%In%" THEN "X"
+        ELSE "O"
+    END as 중성화
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID
